de21f81bc568be88e51c3c2f5beb9e75bfdc7db1
meneerhop <mitchellhop@hotmail.com>
2026-07-29 15:13:26 +0200

    Add Supabase credentials

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@
    Vervang JOUW_PROJECT en JOUW_ANON_KEY met jouw Supabase gegevens
    ============================================================ */
 
-const SUPABASE_URL        = "https://JOUW_PROJECT.supabase.co/rest/v1/";
-const SUPABASE_PROJECT_URL = "https://JOUW_PROJECT.supabase.co";
-const SUPABASE_ANON_KEY   = "JOUW_ANON_KEY";
+const SUPABASE_URL        = "https://rizuafrymcgeffdmfnlt.supabase.co/rest/v1/";
+const SUPABASE_PROJECT_URL = "https://rizuafrymcgeffdmfnlt.supabase.co";
+const SUPABASE_ANON_KEY   = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpenVhZnJ5bWNnZWZmZG1mbmx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzMjUyMDgsImV4cCI6MjEwMDkwMTIwOH0.PhOTF-jR3c57yoraPUV-i2S-rvUjrW-CChuMDiDqQDY";
 const SITE_PIN            = "2853";
 const TEAM_NAAM           = "ASC Nieuwland 3";
